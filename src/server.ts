445ed572706a6ee26c9d4fc7641288c86cc43e33
Samuel-Galassi <sgcontatodireto@gmail.com>
2026-05-04 21:02:40 -0300

    server.ts added host on app.listen

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const startServer = async () => {
 
     await initializeGlobalCategories();
 
-    await app.listen({ port: PORT }).then(() => {
+    await app.listen({ port: PORT, host: "0.0.0.0" }).then(() => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (err) {
